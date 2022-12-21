95e29c91984d72f77e495f9d10006ea06bd16377
Gabriel Diniz <gabrieldiniz.contato@gmail.com>
2022-12-21 18:25:44 -0300

    Service layer, component registration

--- src/main/java/com/gabrieldiniz/webservices/repositories/UserRepository.java
+++ src/main/java/com/gabrieldiniz/webservices/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.gabrieldiniz.webservices.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.gabrieldiniz.webservices.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
