c2fe5ea35f539527c02b2f17e6f7affabcbf9759
Gabriel Diniz <gabrieldiniz.contato@gmail.com>
2022-12-22 13:36:25 -0300

    Category

--- src/main/java/com/gabrieldiniz/webservices/services/UserService.java
+++ src/main/java/com/gabrieldiniz/webservices/services/UserService.java
@@ -20,7 +20,7 @@ public class UserService {
 	}
 	
 	public User findById(Long id) {
-		Optional<User>obj =repository.findById(id);
+		Optional <User> obj = repository.findById(id);
 		return obj.get();
 	}
 }
